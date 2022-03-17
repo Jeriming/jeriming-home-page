9d3a4a3f370add6bc09c618d36505da83584f8c8
Jeriming <316906528@qq.com>
2022-03-17 21:46:02 +0800

    empty template

---
+++ src/components/SimpleWater.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- WebGL核心概念：gl_Position和gl_FragColor的计算 -->
+  <!-- 即：顶点坐标的计算和片元颜色的计算，前者决定物体的形状和远近（摄像机），后者决定物体的颜色（包括光照反应） -->
+  <div></div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
